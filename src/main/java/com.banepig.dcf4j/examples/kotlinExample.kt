ca715d412def73864786d4d6838f6ee7b613e86b
BanePig <thegosalexander@gmail.com>
2018-07-25 06:52:43 -0400

    Update kotlinExample.kt

--- src/main/java/com.banepig.dcf4j/examples/kotlinExample.kt
+++ src/main/java/com.banepig.dcf4j/examples/kotlinExample.kt
@@ -1,7 +1,9 @@
-// An example of how to use Optional parameters
-@Command(label="!purge", usage = "!purge [Amount]", description = "Bulk-deletes messages.")
-fun handlePing(message: IMessage, @Optional amount: Long?) {
-    if(amount == null) amount = 5;
-    var history = message.channel.getMessageHistoryFrom(amount);
-    history.bulkDelete();
+class purgeCommand {
+    // An example of how to use Optional parameters
+    @Command(label="!purge", usage = "!purge [Amount]", description = "Bulk-deletes messages.")
+    fun handlePing(message: IMessage, @Optional amount: Long?) {
+        if(amount == null) amount = 5;
+        var history = message.channel.getMessageHistoryFrom(amount);
+        history.bulkDelete();
+    }
 }
