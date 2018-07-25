67d084bb0a860c8728b0c049121516e4cdf0f7c8
BanePig <thegosalexander@gmail.com>
2018-07-25 07:10:29 -0400

    Added getter and setter for invalid usage message.

--- src/main/java/com.banepig.dcf4j/examples/kotlinExample.kt
+++ src/main/java/com.banepig.dcf4j/examples/kotlinExample.kt
@@ -1,9 +1,14 @@
+import com.banepig.dcf4j.Command
+import com.banepig.dcf4j.Optional
+import sx.blah.discord.handle.obj.IMessage
+
 class purgeCommand {
     // An example of how to use Optional parameters
     @Command(label="!purge", usage = "!purge [Amount]", description = "Bulk-deletes messages.")
     fun handlePing(message: IMessage, @Optional amount: Long?) {
-        if(amount == null) amount = 5;
-        var history = message.channel.getMessageHistoryFrom(amount);
-        history.bulkDelete();
+        var finalAmount = amount
+        if (amount == null) finalAmount = 5
+        var history = message.channel.getMessageHistoryFrom(finalAmount!!)
+        history.bulkDelete()
     }
 }
