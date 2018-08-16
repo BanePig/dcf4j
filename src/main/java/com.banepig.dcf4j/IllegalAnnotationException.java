d00b4113ef9d5f32f66ee173fb9ebf220749c774
BanePig <thegosalexander@gmail.com>
2018-08-15 21:53:29 -0400

    Fixed bug

--- src/main/java/com.banepig.dcf4j/IllegalAnnotationException.java
+++
@@ -1,8 +0,0 @@
-package com.banepig.dcf4j;
-
-/**
- * Thrown when a required parameter is found after an optional one.
- */
-public class IllegalAnnotationException extends RuntimeException {
-
-}
--- src/main/java/com.banepig.dcf4j/stringcaster/CastType.java
+++
@@ -1,5 +0,0 @@
-package com.banepig.dcf4j.stringcaster;
-
-public enum CastType {
-    IUSER, ICHANNEL, LONG, DOUBLE, STRING
-}
